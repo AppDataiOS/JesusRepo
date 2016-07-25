11e8fc7e26b157628d5f2b387fc90286f1619abe
chucola thomes <AlejandroGutierrez@chucolas-MacBook-Pro.local>
2016-07-25 14:32:15 -0500

    agregado archivo playground

---
+++ EjercicioPlayground/jesus.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str = "sdf"
+
+var string1 = "Variable tipo string"
+var vectorDeCaracteres = ["a","b","c"]
+var vectorDeNumeros = [1,2,3]
+var Diccionario1 = ["nombre" : "fulano"]
+                   ["apellido" : "perez"]
+
+let cadena:String
+cadena =  "aasa"
+
